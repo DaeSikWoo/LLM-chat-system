ade1bda725fcbf5236fa75fa525c89b6fe8c9a14
Danny Woo <35883117+DaeSikWoo@users.noreply.github.com>
2023-06-16 10:32:01 +0900

    Update service.py

--- service.py
+++ service.py
@@ -8,7 +8,7 @@ import click
 from constants import CHROMA_SETTINGS, PERSIST_DIRECTORY
 
 def load_model():
-    model_id = "TheBloke/vicuna-7B-1.1-HF"
+    model_id = "TheBloke/vicuna-7B-1.1-HF" #set language model
     tokenizer = LlamaTokenizer.from_pretrained(model_id)
     model = LlamaForCausalLM.from_pretrained(model_id)
     pipe = pipeline(
@@ -24,7 +24,7 @@ def load_model():
     return local_llm
 
 def initialize_qa():
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl", model_kwargs={"device": "cpu"}) #cuda for GPU
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl", model_kwargs={"device": "cpu"}) # set resource : cuda for GPU
     db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
     retriever = db.as_retriever()
     loaded_model = load_model()
@@ -32,7 +32,7 @@ def initialize_qa():
     return qa
 
 @click.command()
-@click.option('--device_type', default='cpu', help='device to run on') #cuda for GPU
+@click.option('--device_type', default='cpu', help='device to run on') # cuda for GPU
 def main(device_type):
     initialize_qa()
 
